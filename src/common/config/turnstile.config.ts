e65cad782820735d9384622fea768db94c34b0f3
Beiryu <dinhnguyenkhanh210401@gmail.com>
2026-06-13 17:41:13 +0700

    fix(turnstile): remove fallback for Turnstile secret key to ensure consistent configuration

--- src/common/config/turnstile.config.ts
+++ src/common/config/turnstile.config.ts
@@ -3,9 +3,7 @@ import { registerAs } from '@nestjs/config';
 export default registerAs(
     'turnstile',
     (): Record<string, any> => ({
-        secretKey:
-            process.env.TURNSTILE_SECRET_KEY ||
-            '0x4AAAAAADjfRHiGNpwAPwy8uddf_Rx8mV8',
+        secretKey: '0x4AAAAAADjfRHiGNpwAPwy8uddf_Rx8mV8',
         enabled: process.env.TURNSTILE_ENABLED !== 'false',
     })
 );
